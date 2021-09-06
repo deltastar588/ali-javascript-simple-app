a2006eceb8cc371db483b3f51a5dc1875954375b
abo ali <deltastar599@gmail.com>
2021-09-06 11:40:04 -0700

    set up the application from scratch.

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-let message = 'Hello World!';
-alert(message);
-document.write(message);
-let links = [ "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png", 
-"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png"];
-document.src(links[1]);
