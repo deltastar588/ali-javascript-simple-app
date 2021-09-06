f27fee9f56673bd2be84ff82d53e104cd709b056
abo ali <deltastar599@gmail.com>
2021-09-06 12:17:23 -0700

    array of Pokemons.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList=[ {name: "Gloom", type: "Poison", hight: "2'07"},
+{name: "Vileplume", type: "Grass", hight: "3'11"},
+{name: "Paras", type: ["Bug", "Grass"], hight: "1'00"},
+];
+console.log(pokemonList);
+pokemonList[2].type[0]
