66afbeca2272928d447eae63179a977244509d1e
abo ali <deltastar599@gmail.com>
2021-09-07 13:07:27 -0700

    Array of Pokemons commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
-let pokemonList=[ {name: "Gloom", type: "Poison", hight: "2'07"},
-{name: "Vileplume", type: "Grass", hight: "3'11"},
-{name: "Paras", type: ["Bug", "Grass"], hight: "1'00"},
+
+let pokemonList=[ { name: "Gloom", type: "Poison", hight: 2.07, ImgUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/044.png" },
+{name: "Vileplume", type: "Grass", hight: 3.11 , ImgUrl:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/045.png"},
+{name: "Paras", type: ["Bug", "Grass"], hight: 1 , ImgUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/046.png"},
 ];
-console.log(pokemonList);
-pokemonList[2].type[0]
+
+for (let i=0; i < pokemonList.length; i++){
+    document.write( " " + pokemonList[i].name +"  " + pokemonList[i].hight + "<br>");
+    document.write(`<img src = "${pokemonList[i].ImgUrl}" />`)
+    if (pokemonList[i].hight > 3.0){
+        console.log(pokemonList[i].name + "" +" It is the biggest Pokemons" );
+        document.write("This is the biggest Pokemons <br>");   
+    
+    }  else if (pokemonList[i].hight > 1 && pokemonList[i].hight < 3.0){
+        console.log(pokemonList[i].name + "" +" It is the medium Pokemons" );
+        document.write("This Pokemon is Medium <br>");
+        
+    } else {
+        console.log(pokemonList[i].name + "" +" It is the smallest Pokemons" );
+        document.write("This is smallest<br>");
+    
+    }
+}
+
+
+
